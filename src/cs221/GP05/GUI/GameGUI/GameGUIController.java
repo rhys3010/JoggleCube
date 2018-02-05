6c42733a27d7ee3a3be7f333582cdb5071e1b802
rhe24 <rhe24@aber.ac.uk>
2018-02-05 22:13:46 +0000

    Updated Pause Button behaviour

--- src/cs221/GP05/GUI/GameGUI/GameGUIController.java
+++ src/cs221/GP05/GUI/GameGUI/GameGUIController.java
@@ -14,6 +14,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -25,19 +27,40 @@ import java.io.IOException;
  * Used with GameGUI.fxml
  * todo improve this description
  * @author Nathan Williams (naw21)
+ * @author Rhys Evans (rhe24@aber.ac.uk)
  * @version 0.1  DRAFT
  */
 public class GameGUIController {
     /**
-     * the parent VBox in this scene.
+     * the parent StackPane in this scene.
      */
     @FXML
-    VBox parent;
+    StackPane parent;
+
+    /**
+     * The pause overlay
+     */
+    @FXML
+    VBox pauseOverlay;
+
+    /**
+     * The main game screen content (cube I/O etc)
+     */
+    @FXML
+    VBox gameScreen;
+
+    /**
+     * All the pause menu buttons
+     */
+    @FXML
+    Button resumeBtn, exitBtn, helpBtn;
+
 
     /**
      * When the End Game button is clicked it will load the startGUI scene.
      *
      * todo display score and save options instead of startGUI
+     * todo change to new 'end scene'
      *
      * @author Nathan Williams (naw21)
      * @version 0.1 DRAFT
@@ -51,4 +74,54 @@ public class GameGUIController {
         stage.setScene(new Scene(root,600,600));
         //todo call a method to stop the game running.
     }
+
+
+    /**
+     * When the pause button is clicked it will display an overlay and pause the game
+     * @author Rhys Evans (rhe24@aber.ac.uk)
+     * @version 0.1 DRAFT
+     */
+    @FXML
+    void btnPauseGameClicked(){
+
+        // Disable game screen and show overlay
+        pauseOverlay.setVisible(true);
+        gameScreen.setDisable(true);
+    }
+
+    /**
+     * When the resume button is pressed, remove overlay and enable game screen
+     * @author Rhys Evans (rhe24@aber.ac.uk)
+     * @version 0.1 DRAFT
+     */
+    @FXML
+    void btnResumeClicked(){
+
+        // Enable game screen and hide overlay
+        pauseOverlay.setVisible(false);
+        gameScreen.setDisable(false);
+    }
+
+    /**
+     * When the resume button is pressed, remove overlay and enable game screen
+     * todo fix dodgy model overlaying
+     * todo add prompt for exiting
+     *
+     * @author Rhys Evans (rhe24@aber.ac.uk)
+     * @version 0.1 DRAFT
+     * @throws IOException if the StartGUI.fxml is not found.
+     */
+    @FXML
+    void btnExitClicked() throws IOException{
+
+        // Copied code right over from other method, will refine and change when I add a END Scene
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("../StartGUI/StartGUI.fxml"));
+        Stage stage = (Stage) parent.getScene().getWindow();
+        stage.setScene(new Scene(root,600,600));
+    }
+
+
+
 }
--- src/cs221/GP05/JoggleCube.java
+++ src/cs221/GP05/JoggleCube.java
@@ -30,7 +30,21 @@ import java.io.IOException;
  *          todo make more GUIs: HighScore, pause, Loading etc read the spec.
  *          todo link game logic to GUI
  *          todo write documentation
+ *          todo fix proportions in GameGUI, bigger cube? (can come back to this after model is sorted)
+ *
+ *          todo add flair to design and house style (low priority)
+ *              - logo/banner?
+ *              - start screen
+ *
+ *          todo add 'recents' list to previously played cubes
+ *
  *          lol that was easy.
+ *
+ *      Changelog:
+ *          > Made a changelog (meta right?) - rhe24 - 5/2/2018
+ *          > Changed window title to JoggleCube - rhe24 - 5/2/2018
+ *          > Added text field for manual word entry - rhe24 - 5/2/2018
+ *
  *
  *      Hints and Suggestions:
  *          When writing code comment everythingL
@@ -39,6 +53,7 @@ import java.io.IOException;
  *
  *
  * @author Nathan Williams (naw21)
+ * @author Rhys Evans (rhe24@aber.ac.uk)
  * @version 0.1  DRAFT
  */
 public class JoggleCube extends Application {
@@ -54,7 +69,7 @@ public class JoggleCube extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("GUI/StartGUI/StartGUI.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("JoggleCube");
         primaryStage.setScene(new Scene(root, 600, 600));
         //todo add methods to launch model, loading dictionary, Highscore data etc.
         //todo maybe a loading screen while this happens.
