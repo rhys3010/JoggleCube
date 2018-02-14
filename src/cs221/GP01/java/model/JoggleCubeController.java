b3edd855992683bf508550db10cd3907932f8711
Unknown <samjones714@gmail.com>
2018-02-14 19:07:02 +0000

    Started to implement the Interfaces
    - Added the Dictionary Interface
    - Added the JoggleCubeController Interface

---
+++ src/cs221/GP01/java/model/JoggleCubeController.java
@@ -0,0 +1,17 @@
+package cs221.GP01.java.model;
+
+/**
+ * @author Samuel Jones - srj12
+ * @version 0.1
+ */
+
+public interface JoggleCubeController {
+
+    private void loadDictionary(String DictionaryFileName){}
+
+    private void loadHighScores(String highScoreFileName){}
+
+    private void saveHighScores(String highScoreFileName){}
+
+    public void 
+}
