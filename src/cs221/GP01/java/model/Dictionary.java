0a590eafa3c806fe8e0658cc119c9f0c62c4758c
Nathan <naw21@aber.ac.uk>
2018-02-14 19:48:39 +0000

    fixed Interface

--- src/cs221/GP01/java/model/Dictionary.java
+++ src/cs221/GP01/java/model/Dictionary.java
@@ -10,18 +10,18 @@ public interface Dictionary {
      * Loads the dictionary into the local hash map handled by the object that is dictionary using the filename given
      * @param filename Finds the file named as the String parameter
      */
-    public void loadDictionary(String filename);
+    void loadDictionary(String filename);
 
     /**
      * Searches the dictionary utilising the has map for the word and returns true if the word is present false if not
      * @param word the String that is being searched for in the dictionary
      * @return Returns true if the word is present, false if the word is not.
      */
-    public boolean searchDictionary(String word);
+    boolean searchDictionary(String word);
 
     /**
      * Returns the size of the local hash map
      * @return An int equal to the size of the dictionary
      */
-    public int dictionarySize();
+    int getDictionarySize();
 }
--- src/cs221/GP01/java/model/JoggleCubeController.java
+++ src/cs221/GP01/java/model/JoggleCubeController.java
@@ -10,23 +10,23 @@ public interface JoggleCubeController {
      * This handles the loading of the dictionary
      * @param DictionaryFileName The file-name of the dictionary that is saved with the program
      */
-    private void loadDictionary(String DictionaryFileName){}
+    void loadDictionary(String DictionaryFileName);
 
     /**
      * This handles the loading of the high scores
      * @param highScoreFileName The file-name of the high scores that is saved with the program
      */
-    private void loadHighScores(String highScoreFileName){}
+    void loadHighScores(String highScoreFileName);
 
     /**
      * This handles the saving of the High scores to a text file
      * @param highScoreFileName The file-name of the high scores that is loaded with the program
      */
-    private void saveHighScores(String highScoreFileName){}
+    void saveHighScores(String highScoreFileName);
 
     /**
      * This is the main application and handles all of the running of the program for the the main.java file including
      * loading and saving of all data and the interaction of the backend with the UI controllers
      */
-    public void runJoggleCube();
+    void runJoggleCube();
 }
