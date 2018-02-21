e59fa13ce26fdd5b9862e9bb2f0111731b224b44
Unknown <srj12@aber.ac.uk>
2018-02-21 17:01:14 +0000

    UML changes and a few extras

--- src/cs221/GP01/java/model/IBlock.java
+++ src/cs221/GP01/java/model/IBlock.java
@@ -1,4 +1,12 @@
 package cs221.GP01.java.model;
 
+/**
+ * The Interface which holds all the details for the Block
+ * @author Samuel Jones - srj12
+ * @version 0.1
+ */
+
 public interface IBlock {
+    void generateLetter();
+    void setLetter(String letter);
 }
--- src/cs221/GP01/java/model/ICube.java
+++ src/cs221/GP01/java/model/ICube.java
@@ -1,4 +1,26 @@
 package cs221.GP01.java.model;
 
+/**
+ * The Interface which handles and looks after the Cube
+ * @author Samuel Jones - srj12
+ * @version 0.1
+ */
+
 public interface ICube {
+    /**
+     * Returns the block at the given co-ordinates xyz as the block object
+     * @param x
+     * @param y
+     * @param z
+     * @return
+     */
+    Block getBlock(int x, int y, int z);
+
+    /**
+     * Generates the cube using sudo random Java functions in the block objects
+     */
+    void populateCube();
+
+
+    ArrayList<Block*> getNeighbours(int x, int y, int z);
 }
