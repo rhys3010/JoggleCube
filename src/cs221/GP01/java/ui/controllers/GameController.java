1f548a6dceb31bc13435deb6847cbb0a92eae08e
Nathan <naw21@aber.ac.uk>
2018-02-22 23:50:28 +0000

    created 2.5d view. :) someone please change to nicer colours.

--- src/cs221/GP01/java/ui/controllers/GameController.java
+++ src/cs221/GP01/java/ui/controllers/GameController.java
@@ -14,16 +14,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.geometry.HPos;
+import javafx.geometry.Point3D;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -55,7 +55,10 @@ public class GameController implements Initializable{
     private ObservableList<String> foundWords = FXCollections.observableArrayList();
 
     @FXML
-    private GridPane top,middle,bottom;
+    private GridPane top2d,middle2d,bottom2d,top25d,middle25d,bottom25d;
+
+    @FXML
+    private Button btnSubmit;
 
 
     /**
@@ -82,15 +85,23 @@ public class GameController implements Initializable{
 
 
     private Label[][][] labelCube;
+    private Box[][][] boxCube;
 
 
-    void CubeClicked(int k,int i,int j,Label label){
+    /**
+     * When a block is clicked this method is called.
+     *
+     * @param k the position of the block that called the method
+     * @param i the position of the block that called the method
+     * @param j the position of the block that called the method
+     */
+    private void blockClicked(int k, int i, int j){
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
                 for (int z = 0; z < 3; z++) {
                     if(x == k && y == i && z == j){
                         setSelected(x,y,z);
-                    } else if(neighbour(k,i,j,x,y,z)){
+                    } else if(isNeighbour(k,i,j,x,y,z)){
                         setActive(x,y,z,false);
                     } else {
                         setInActive(x,y,z);
@@ -98,72 +109,189 @@ public class GameController implements Initializable{
                 }
             }
         }
-        textField.appendText(label.getText());
+        textField.appendText(labelCube[k][i][j].getText());
     }
+
+    /**
+     * sets the state of the block to selected
+     *
+     * @param x position of the block in question
+     * @param y position of the block in question
+     * @param z position of the block in question
+     */
 
     private void setSelected(int x, int y, int z) {
         labelCube[x][y][z].setStyle("-fx-background-color:#ff0000;");
         labelCube[x][y][z].setOnMouseClicked(null);
+
+        boxCube[x][y][z].setMaterial(generateMaterial(labelCube[x][y][z].getText(),"#ff0000"));
+        boxCube[x][y][z].setOnMouseClicked(null);
     }
+
+
+    /**
+     * sets the state of the block to in-active
+     *
+     * @param x position of the block in question
+     * @param y position of the block in question
+     * @param z position of the block in question
+     */
 
     private void setInActive(int x, int y, int z) {
         if(!labelCube[x][y][z].getStyle().contains("-fx-background-color:#550000;")){
             labelCube[x][y][z].setStyle("-fx-background-color:#566377;");
             labelCube[x][y][z].setOnMouseClicked(null);
-        }
-    }
 
-    private void setActive(int x, int y, int z, boolean overide) {
-        if(labelCube[x][y][z].getStyle().contains("-fx-background-color:#ff0000;") && !overide){
-            labelCube[x][y][z].setStyle("-fx-background-color:#550000;");
-        } else if(!labelCube[x][y][z].getStyle().contains("-fx-background-color:#550000;") || overide) {
-            labelCube[x][y][z].setStyle("-fx-background-color:#2980b9;");
-            labelCube[x][y][z].setOnMouseClicked(e -> CubeClicked(x, y, z, labelCube[x][y][z]));
+            boxCube[x][y][z].setMaterial(generateMaterial(labelCube[x][y][z].getText(),"#566377"));
+            boxCube[x][y][z].setOnMouseClicked(null);
         }
     }
 
     /**
-     * Initialize game screen
+     * sets the state of the block to Active allowing the user to click on it
+     *
+     * @param x position of the block in question
+     * @param y position of the block in question
+     * @param z position of the block in question
+     * @param override overides any other states ready to begin selecting again
      */
-    @Override
-    public void initialize(URL location, ResourceBundle resources){
+
+    private void setActive(int x, int y, int z, boolean override) {
+        if(labelCube[x][y][z].getStyle().contains("-fx-background-color:#ff0000;") && !override){
+            labelCube[x][y][z].setStyle("-fx-background-color:#550000;");
+            boxCube[x][y][z].setMaterial(generateMaterial(labelCube[x][y][z].getText(),"#550000"));
+
+        } else if(!labelCube[x][y][z].getStyle().contains("-fx-background-color:#550000;") || override) {
+            labelCube[x][y][z].setStyle("-fx-background-color:#2980b9;");
+            labelCube[x][y][z].setOnMouseClicked(e -> blockClicked(x, y, z));
+
+            boxCube[x][y][z].setMaterial(generateMaterial(labelCube[x][y][z].getText(),"#2980b9"));
+            boxCube[x][y][z].setOnMouseClicked(e -> blockClicked(x, y, z));
+        }
+    }
+
+    private void buildGrids(String[][][] letters) {
+        //creates space ready for the boxes and labels
         labelCube = new Label[3][3][3];
-        //
+        boxCube = new Box[3][3][3];
+
+        //easy access of the three planes
+        GridPane[] twoDGrid = {top2d, middle2d, bottom2d};
+        GridPane[] twoFiveDGrid = {top25d, middle25d, bottom25d};
+
+        //links the ListView with the list
         foundWordsList.setItems(foundWords);
 
+        //stops user editing the textField todo test this
+        textField.setDisable(true);
 
-        String[][][] letters = UIController.getJoggleCube().getCubeData();
+        //checks the letters actually exist before making a fool of ones self and trying to display them.
         if(letters[0][0][0] != null) {
-            GridPane[] twoDGrid = {top, middle, bottom};
+            //Create the labels and boxes ready for display
             for (int k = 0; k < 3; k++) {
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 3; j++) {
+                        Box box = new Box(30, 30, 30);
+                        box.setRotationAxis(new Point3D(0,0,0));
+                        box.setRotate(0);
+                        boxCube[k][i][j] = box;
+
                         Label label = new Label(letters[k][i][j]);
                         labelCube[k][i][j] = label;
+
                         setActive(k,i,j,true);
-                        twoDGrid[k].add(label, i, j);
+                    }
+                }
+            }
+
+            //render the labels and blocks (adds them to relevant grids)
+            for (int k = 0; k < 3; k++) {
+                //rotates the grids ready for display todo move to FXML
+                twoFiveDGrid[k].setRotationAxis(new Point3D(0.3,-1.0,0));
+                twoFiveDGrid[k].setRotate(45);
+
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 2; j > -1; j--) {
+                        twoDGrid[k].add(labelCube[k][j][i], i, j);
+                        twoFiveDGrid[k].add(boxCube[k][j][i],j,i);
                     }
                 }
             }
         }
-        //todo load this data into the other grids for display
-
-
-        //todo create an instance of timer and start it to keep updating
     }
 
-    private boolean neighbour(int f,int g, int h, int a, int b, int c) {
-        int x,y,z;
+
+    /**
+     * generates a material with a letter on it to display a block
+     *
+     * @param letter the letter to display
+     * @param color the background color
+     * @return a material with a letter on it ready to display
+     */
+    private PhongMaterial generateMaterial(String letter,String color) {
+        //Create a container
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+
+        //create a label with the letter
+        Label label = new Label(letter);
+        label.setStyle("-fx-font-size: 22; -fx-text-fill:white;");
+        GridPane.setHalignment(label, HPos.CENTER);
+
+        //add the label to the grid
+        grid.add(label, 0, 0);
+
+        //set the gridlines to show
+        grid.setGridLinesVisible(true);
+
+        //set the column and row sizes iof the grid
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(100);
+        grid.getColumnConstraints().addAll(col1);
+        RowConstraints row1 = new RowConstraints();
+        grid.getRowConstraints().addAll(row1);
+        row1.setPercentHeight(100);
+
+        //set height and width of the grid
+        grid.setPrefSize(30, 30);
+        //give the grid a background colour
+        grid.setStyle("-fx-background-color: "+ color + ";");
+        //create a scene to render the grid.
+        Scene tmpScene = new Scene(grid);
+
+        //take a snapshot of the grid and turn into and image
+        Image net = grid.snapshot(null, null);
+        //create the material
+        PhongMaterial mat = new PhongMaterial();
+        //set the material to show the image
+        mat.setDiffuseMap(net);
+
+        return mat;
+    }
+
+    /**
+     * Works out if the selected blocks are neighbors.
+     *
+     * @param selectedX position of the selected block
+     * @param selectedY position of the selected block
+     * @param selectedZ position of the selected block
+     * @param possibleX position of the possible neighbor block
+     * @param possibleY position of the possible neighbor block
+     * @param possibleZ position of the possible neighbor block
+     * @return whether the two blocks are neighbors or not
+     */
+    private boolean isNeighbour(int selectedX, int selectedY, int selectedZ, int possibleX, int possibleY, int possibleZ) {
+        int neighborsX,neighborsY,neighborsZ;
         for(int i = -1; i<2; i++) {
-            x = f;
-            x += i;
+            neighborsX = selectedX;
+            neighborsX += i;
             for (int j = -1; j < 2; j++) {
-                y = g;
-                y += j;
+                neighborsY = selectedY;
+                neighborsY += j;
                 for (int k = -1; k < 2; k++) {
-                    z = h;
-                    z += k;
-                    if(a == x && b == y && c == z)
+                    neighborsZ = selectedZ;
+                    neighborsZ += k;
+                    if(possibleX == neighborsX && possibleY == neighborsY && possibleZ == neighborsZ)
                         return true;
                 }
             }
@@ -177,8 +305,6 @@ public class GameController implements Initializable{
     @FXML
     private void btnEndGameClicked() {
         UIController.getScreenController().show(ScreenType.END);
-
-        // Backend Example
         UIController.getJoggleCube().endGame();
     }
 
@@ -195,18 +321,24 @@ public class GameController implements Initializable{
                         }
                     }
                 }
-
-
-
-                //todo change the colour of the button to green
+                //todo make these css variables
+                btnSubmit.setStyle("-fx-background-color:#006600;");
             } else {
-                //todo change the colour of the button to red
+                btnSubmit.setStyle("-fx-background-color:#880000;");
             }
         } else {
-            //todo change the colour of the button to red
+            btnSubmit.setStyle("-fx-background-color:#880000;");
         }
 
-
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        btnSubmit.setStyle("-fx-background-color:-fx-tertiary-color;");
+                    }
+                },
+                1000
+        );
     }
 
     //todo add method to change the submit buttons colour back when the the mouse is next clicked maybe?
@@ -218,8 +350,6 @@ public class GameController implements Initializable{
     @FXML
     private void btnPauseGameClicked(){
         UIController.getScreenController().show(ScreenType.PAUSE);
-
-        // Backend Example
         UIController.getJoggleCube().pauseGame();
     }
 
@@ -229,6 +359,15 @@ public class GameController implements Initializable{
     @FXML
     private void btnSettingsClicked(){
         UIController.getScreenController().show(ScreenType.SETTINGS);
+    }
+
+    /**
+     * Initialize game screen
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        buildGrids(UIController.getJoggleCube().getCubeData());
+        //todo create an instance of timer and start it to keep updating th label
     }
 
 
