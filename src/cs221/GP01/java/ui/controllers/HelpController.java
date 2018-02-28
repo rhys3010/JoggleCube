025698a54afb1b9cae03ffa84a0907d8cca79101
rhe24 <rhe24@aber.ac.uk>
2018-02-28 14:11:35 +0000

    Partially added carousel indicators to help overlay

--- src/cs221/GP01/java/ui/controllers/HelpController.java
+++ src/cs221/GP01/java/ui/controllers/HelpController.java
@@ -10,11 +10,16 @@ package cs221.GP01.java.ui.controllers;
 
 import cs221.GP01.java.ui.ScreenType;
 import cs221.GP01.java.ui.UIController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.SubScene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
 import java.net.URL;
@@ -33,7 +38,7 @@ public class HelpController extends BaseOverlayController implements INeedPrep{
     private static final String PAGES_PATH_PREFIX = "../../../resource/view/helppages/";
 
     /**
-     * List to store all the help screens available as JavaFX parent nodes
+     * List to store all the help screens available as FXML parent nodes
      * so they can be injected / removed from the help screen with ease
      */
     private ArrayList<Parent> helpScreens = new ArrayList<>();
@@ -49,6 +54,12 @@ public class HelpController extends BaseOverlayController implements INeedPrep{
     @FXML
     private SubScene helpPageContainer;
 
+    /**
+     * The FXML node of the carousel indicator container
+     */
+    @FXML
+    HBox carouselIndicatorContainer;
+
 
     /**
      * Constructor to ensure UIController object is passed
@@ -58,8 +69,7 @@ public class HelpController extends BaseOverlayController implements INeedPrep{
     public HelpController(UIController UIController) throws IOException{
         super(UIController);
 
-        // Create all the pages as FXMLLoaders
-        // todo: add more pages
+        // Create all the pages as FXML parent nodes
         helpScreens.add(createHelpPage("ExampleHelpPage.fxml"));
         helpScreens.add(createHelpPage("AnotherExampleHelpPage.fxml"));
         helpScreens.add(createHelpPage("LastHelpPageExample.fxml"));
@@ -73,8 +83,16 @@ public class HelpController extends BaseOverlayController implements INeedPrep{
         // Set current index to 0
         currentPageIndex = 0;
 
+        // Create the indicators at the bottom of the screen
+        createCarouselIndicators();
+
+        // Add behaviour to all the carousel indicators
+        for(Node button : carouselIndicatorContainer.getChildren()){
+
+        }
+
         // Inject FXML of first entry in the helpScreens list to the subscene root
-        helpPageContainer.setRoot(helpScreens.get(0));
+        changePage();
     }
 
     
@@ -100,8 +118,7 @@ public class HelpController extends BaseOverlayController implements INeedPrep{
         currentPageIndex %= helpScreens.size();
 
         // Update the page to display the correct screen
-        helpPageContainer.setRoot(helpScreens.get(currentPageIndex));
-
+        changePage();
     }
 
     /**
@@ -118,18 +135,53 @@ public class HelpController extends BaseOverlayController implements INeedPrep{
         currentPageIndex = (((currentPageIndex % helpScreens.size()) + helpScreens.size()) % helpScreens.size());
 
         // Update the page to display the correct screen
-        helpPageContainer.setRoot(helpScreens.get(currentPageIndex));
+        changePage();
+    }
+
+
+    /**
+     * Utility function to create the carouselIndicator icons depending on size of the list
+     */
+    @FXML
+    private void createCarouselIndicators(){
+
+        // Iteratively create a carouselindicator for each help screen
+        for(int i = 0; i < helpScreens.size(); i++){
+            Button newButton = new Button();
+
+            newButton.getStyleClass().add("carouselIndicator");
+            newButton.setMinSize(16, 16);
+
+            carouselIndicatorContainer.getChildren().add(newButton);
+        }
     }
 
     /**
      * Utility function to create the help pages as an FXMLLoader
      * @param name - the name of the FXML file
-     * @return loader - the created FXML loader
+     * @return loader - the created FXML parent node
      */
     private Parent createHelpPage(String name) throws IOException{
         FXMLLoader loader = new FXMLLoader(getClass().getResource(PAGES_PATH_PREFIX + name));
 
         return loader.load();
+    }
+
+    /**
+     * Utility function to change the page that is currently displayed
+     */
+    private void changePage(){
+
+        // Set the FXML root
+        helpPageContainer.setRoot(helpScreens.get(currentPageIndex));
+
+        // De-select other carousel indicators
+        for(int i = 0; i < carouselIndicatorContainer.getChildren().size(); i++){
+            carouselIndicatorContainer.getChildren().get(i).getStyleClass().remove("carouselIndicator-selected");
+        }
+
+        // Update the carousel indicator
+        carouselIndicatorContainer.getChildren().get(currentPageIndex).getStyleClass().add("carouselIndicator-selected");
     }
 
 }
