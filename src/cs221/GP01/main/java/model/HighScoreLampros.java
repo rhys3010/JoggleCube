26625b26fac81344cd73d80dc8f5a2099b62e03b
Unknown <srj12@aber.ac.uk>
2018-03-08 10:05:32 +0000

    Added the work from Lampros

---
+++ src/cs221/GP01/main/java/model/HighScoreLampros.java
@@ -0,0 +1,19 @@
+package cs221.GP01.main.java.model;
+
+public class HighScoreLampros {
+    private String name;
+    private int score;
+    private int position;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public int getPos(){
+        return position;
+    }
+}
---
+++ src/cs221/GP01/main/java/model/HighScores.java
@@ -0,0 +1,10 @@
+package cs221.GP01.main.java.model;
+
+import java.util.ArrayList;
+
+public class HighScores {
+    public ArrayList <HighScore> highScores;
+    public void loadHighScores(){
+        
+    }
+}
