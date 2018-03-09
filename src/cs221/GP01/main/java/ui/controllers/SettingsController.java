0e16f3c7dcf75403afb3ba709cabd0de15ddd48f
rhe24 <rhe24@aber.ac.uk>
2018-03-09 12:21:56 +0000

    Added are you sure overlay to high scores

--- src/cs221/GP01/main/java/ui/controllers/SettingsController.java
+++ src/cs221/GP01/main/java/ui/controllers/SettingsController.java
@@ -4,9 +4,12 @@ import cs221.GP01.main.java.ui.UIController;
 import cs221.GP01.main.java.ui.ScreenType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class SettingsController extends BaseOverlayController implements INeedPrep, Initializable {
@@ -27,6 +30,26 @@ public class SettingsController extends BaseOverlayController implements INeedPr
     @FXML
     public void closeBtnClicked(){
         UIController.getNavigationController().hideOverlay(ScreenType.SETTINGS, parentController);
+    }
+
+    /**
+     * Handle the clearing of highscores
+     */
+    @FXML
+    public void clearHighScoreClicked(){
+        // Display 'are you sure' overlay
+        Alert sureAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        sureAlert.setTitle("Quit Game");
+        sureAlert.setHeaderText(null);
+        sureAlert.setContentText("Are you sure you want to clear all High Scores?");
+
+        Optional<ButtonType> result = sureAlert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            // todo: Clear high scores here
+        } else {
+            sureAlert.close();
+        }
     }
 
     @Override
