dbef3307d32e689606c113fb98effff106343170
rhe24 <rhe24@aber.ac.uk>
2018-03-12 18:18:08 +0000

    Added leading 0 formatting to timer

--- src/cs221/GP01/main/java/model/GameTimer.java
+++ src/cs221/GP01/main/java/model/GameTimer.java
@@ -58,7 +58,7 @@ public class GameTimer implements IGameTimer, Runnable {
             }
             timeLeft = timeLeft - 1;
             currentTime = Duration.ofSeconds(timeLeft);
-            Platform.runLater(() -> timerLabel.setText(currentTime.getSeconds()/60 +":" + currentTime.getSeconds() % 60));
+            Platform.runLater(() -> timerLabel.setText(String.format("%2d:%02d", currentTime.getSeconds()/60, currentTime.getSeconds() % 60)));
             if(interupt){
                 break;
             }
