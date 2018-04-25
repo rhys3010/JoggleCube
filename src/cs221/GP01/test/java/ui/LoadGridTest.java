84ba58a5a97db4754a3761fdb7a597234b7db32b
alm82 <alm82@aber.ac.uk>
2018-04-25 21:22:45 +0100

    More UI tests

--- src/cs221/GP01/test/java/ui/LoadGridTest.java
+++ src/cs221/GP01/test/java/ui/LoadGridTest.java
@@ -9,22 +9,36 @@ import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
 
 import java.net.URL;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoadGridTest extends ApplicationTest {
 
     Parent rootNode;
+    LoadGrid load = LoadGrid.getInstance();
     @BeforeEach
     void setUp() {
+        Start.getInstance().btnLoadGridClicked();
+    }
+
+    @AfterEach
+    public void afterEachTest() throws TimeoutException {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
     }
 
     @Override
@@ -34,11 +48,14 @@ class LoadGridTest extends ApplicationTest {
 
     }
 
+    public void setRootNode(){
+        rootNode = Navigation.getInstance().getMain().getRoot();
+    }
+
     @Test
     void prepView() {
 
-        Start.getInstance().btnLoadGridClicked();
-        rootNode = Navigation.getInstance().getMain().getRoot();
+        setRootNode();
         ListView<String> items = from(rootNode).lookup("#listViewRecents").query();
         assertEquals(JoggleCube.getInstance().getRecentGrids(), items.getItems());
 
@@ -47,12 +64,13 @@ class LoadGridTest extends ApplicationTest {
     @Test
     void btnStartGridClicked() {
 
-        Start.getInstance().btnLoadGridClicked();
-        ListView list = from(rootNode).lookup("#listViewRecents").query();
+        setRootNode();
+        ListView<String> list = from(rootNode).lookup("#listViewRecents").query();
         list.getSelectionModel().selectFirst();
-        LoadGrid.getInstance().btnStartGridClicked();
-
-        rootNode = Navigation.getInstance().getMain().getRoot();
+        load.handleMouseClick();
+        load.btnStartGridClicked();
+// deal with dialog thing
+        setRootNode();
         HBox box = from(rootNode).lookup("#gameBox").query();
         assertEquals("gameElementsContainer",box.getStyle() );
     }
@@ -60,16 +78,30 @@ class LoadGridTest extends ApplicationTest {
     @Test
     void btnBackClicked() {
 
-        Start.getInstance().btnLoadGridClicked();
-        LoadGrid.getInstance().btnBackClicked();
+        LoadGrid.getInstance().btnBackClicked();  // why doesn't work with load instead LoadGrid.getInstance()?
 
-        rootNode = Navigation.getInstance().getMain().getRoot();
-        Label pageType = from(rootNode).lookup("#pageType").query();
+        setRootNode();
+        Label pageType = from(rootNode).lookup("#title").query();
         assertEquals("JoggleCube",pageType.getText() );
 
     }
 
     @Test
     void handleMouseClick() {
+
+        setRootNode();
+        ListView<String> list = from(rootNode).lookup("#listViewRecents").query();
+        list.getSelectionModel().selectFirst();
+        load.handleMouseClick();
+
+        assertNotNull(load.getFileName());
+        assertEquals("grid_1",load.getFileName());
+
+        list.getSelectionModel().select(2);
+        load.handleMouseClick();
+
+        assertEquals("grid_3", load.getFileName() );
+
+
     }
 }
