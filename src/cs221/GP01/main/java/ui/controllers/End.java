35d170e9a543549e6d2995e77af0d28aca8d564b
Unknown <alt38@aber.ac.uk>
2018-05-01 14:04:46 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/cs221/GP01/main/java/ui/controllers/End.java
+++ src/cs221/GP01/main/java/ui/controllers/End.java
@@ -131,6 +131,13 @@ public class End extends BaseOverlay implements INeedPrep {
         }
 
     }
+
+    //@Test
+    //public void testPrepView() {
+       // prepView();
+        //assertEquals(JoggleCube.getInstance().getScore() + "", scoreLabel.getText());
+        //assertEquals(JoggleCube.getInstance().getHighestScore() + "", highScoreLabel.getText());
+    //}
     //agl6
 
     public String getScore() {
