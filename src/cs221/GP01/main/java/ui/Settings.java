e0e490f23661cb02b9fb29851b7d9aa7bf9a1af7
Cameron <cameronhumphreys1997@gmail.com>
2018-05-03 15:22:23 +0100

    Javadoc updates part III

--- src/cs221/GP01/main/java/ui/Settings.java
+++ src/cs221/GP01/main/java/ui/Settings.java
@@ -17,15 +17,15 @@ import java.util.Optional;
 
 /**
  * SettingsOverlay - Class to handle the preferred game configuration
- * <p>
  * Handle the settings of the game and adapt them depending on user input in the settings overlay
+ *
  * @author Rhys Evans
  * @author Nathan Williams (naw21)
  * @version 1.1
  * @see ISettings
  * @see SettingsOverlay
  */
-public class Settings implements ISettings{
+public class Settings implements ISettings {
 
     /**
      * The instance of the settings singleton class
@@ -41,7 +41,7 @@ public class Settings implements ISettings{
      * Available grid languages
      */
 
-    private static String languages[] = {"English","Cymraeg"};
+    private static String languages[] = {"English", "Cymraeg"};
 
     /**
      * Currently used grid language
@@ -61,49 +61,18 @@ public class Settings implements ISettings{
     private String alreadySelectedColor = "#64846b";
     private String unavailableColor = "#aeaeae";
 
-
     /**
      * Private Constructor for settings class so only oone instance of class can be created
-     *
      */
-    private Settings(){}
-
-    public static String getCurrLang() {
-        return currLang;
+    private Settings() {
     }
-
-    public static void setCurrLang(String currLang) {
-        if((currLang.length() == 2)){
-            //converts prefix into full language name
-            for(String lang : languages){
-                if (lang.substring(0,2).toLowerCase().equals(currLang.toLowerCase())) {
-                    Settings.currLang = lang;
-                    break;
-                }
-            }
-        } else {
-            //checks if valid language
-            for(String lang : languages){
-                if (lang.toLowerCase().equals(currLang.toLowerCase())) {
-                    Settings.currLang = lang;
-                    break;
-                }
-            }
-        }
-        //when language is changed set the joggleCubeLanguage
-        JoggleCube.getInstance().setLanguage();
-    }
-
-    public static String getCurrLangPrefix() {
-        return currLang.substring(0,2).toLowerCase();
-    }
-
 
     /**
      * Check if the colour blind option is enabled
+     *
      * @return true/false depending on state of the option
      */
-    public boolean isColorBlindEnabled(){
+    public boolean isColorBlindEnabled() {
         return colorBlind;
     }
 
@@ -128,22 +97,19 @@ public class Settings implements ISettings{
             alreadySelectedColor = "#64846b";
             unavailableColor = "#aeaeae";
         }
-
         // Update Icon
         GameView.getInstance().getColorBlindIcon().setVisible(colorBlind);
     }
 
-
     /**
      * Clear the highscores and prompt user
      */
-    public void clearHighScores(){
-
+    public void clearHighScores() {
         // Create confirmation dialog and store result
         Dialog confirmDialog = new Dialog();
         Optional<ButtonType> result = confirmDialog.showConfirmationDialog("Clear High Scores", "Are you sure you want to clear all High Scores?");
 
-        if(result.get() == ButtonType.OK){
+        if (result.get() == ButtonType.OK) {
             // Remove overall highscores
             JoggleCube.getInstance().clearHighScores();
             Dialog infoDialog = new Dialog();
@@ -151,16 +117,24 @@ public class Settings implements ISettings{
         }
     }
 
-
+    /**
+     * Gets language that is curently set
+     *
+     * @return currLang
+     */
+    public static String getCurrLang() {
+        return currLang;
+    }
 
     /**
      * Returns an instance of the singleton class settings
+     *
      * @return settings
      */
-    public static ISettings getInstance(){
-        if(settings == null){
-            synchronized (Settings.class){
-                if(settings == null){
+    public static ISettings getInstance() {
+        if (settings == null) {
+            synchronized (Settings.class) {
+                if (settings == null) {
                     settings = new Settings();
                 }
             }
@@ -168,48 +142,102 @@ public class Settings implements ISettings{
         return settings;
     }
 
-
+    /**
+     * Get list of languages used by program
+     *
+     * @return list of dictionaries
+     */
     public static String[] getLanguages() {
         return languages;
     }
 
+    /**
+     * Get time remaining on game timer
+     *
+     * @return timerLength - time left on clock
+     */
     public static int getTimerLength() {
         return timerLength;
     }
 
-    public static void setTimerLength(int timerLength) {
-        Settings.timerLength = timerLength;
+    /**
+     * Returns current dictionary language as the two letter prefix (en & cy)
+     *
+     * @return currLang (en or cy)
+     */
+    public static String getCurrLangPrefix() {
+        return currLang.substring(0, 2).toLowerCase();
     }
 
     /**
      * Get the colour to represent currently selected cube
-     * @return
+     *
+     * @return currenlySelectedColor
      */
-    public String getCurrentlySelectedColor(){
+    public String getCurrentlySelectedColor() {
         return currentlySelectedColor;
     }
 
     /**
      * Get the colour to represent available cubes
-     * @return
+     *
+     * @return availableColor
      */
-    public String getAvailableColor(){
+    public String getAvailableColor() {
         return availableColor;
     }
 
     /**
      * Get the colour to represent already selected cubes
-     * @return
+     *
+     * @return alreadySelectedColor
      */
-    public String getAlreadySelectedColor(){
+    public String getAlreadySelectedColor() {
         return alreadySelectedColor;
     }
 
     /**
      * Get the colour to represent unavailable cubes
-     * @return
+     *
+     * @return unavailableColor
      */
-    public String getUnavailableColor(){
+    public String getUnavailableColor() {
         return unavailableColor;
+    }
+
+    /**
+     * Sets length of game timer
+     *
+     * @param timerLength length to be set
+     */
+    public static void setTimerLength(int timerLength) {
+        Settings.timerLength = timerLength;
+    }
+
+    /**
+     * Sets the language of the dictionary
+     *
+     * @param currLang lanuage to be set
+     */
+    public static void setCurrLang(String currLang) {
+        if ((currLang.length() == 2)) {
+            //converts prefix into full language name
+            for (String lang : languages) {
+                if (lang.substring(0, 2).toLowerCase().equals(currLang.toLowerCase())) {
+                    Settings.currLang = lang;
+                    break;
+                }
+            }
+        } else {
+            //checks if valid language
+            for (String lang : languages) {
+                if (lang.toLowerCase().equals(currLang.toLowerCase())) {
+                    Settings.currLang = lang;
+                    break;
+                }
+            }
+        }
+        //when language is changed set the joggleCubeLanguage
+        JoggleCube.getInstance().setLanguage();
     }
 }
