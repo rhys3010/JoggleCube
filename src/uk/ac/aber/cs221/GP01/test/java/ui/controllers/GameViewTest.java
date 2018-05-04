41f5e06f4cb6648a185a5643b691e501f8b64107
agata <agl6@aber.ac.uk>
2018-05-04 11:17:26 +0100

    gameviewtest

--- src/uk/ac/aber/cs221/GP01/test/java/ui/controllers/GameViewTest.java
+++ src/uk/ac/aber/cs221/GP01/test/java/ui/controllers/GameViewTest.java
@@ -1,86 +1,113 @@
 package uk.ac.aber.cs221.GP01.test.java.ui.controllers;
 
-import uk.ac.aber.cs221.GP01.main.java.ui.controllers.GameView;
-import uk.ac.aber.cs221.GP01.main.java.ui.controllers.GridDisplayer;
-import javafx.geometry.Point2D;
+
+import javafx.application.Platform;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.testfx.framework.junit5.ApplicationTest;
+import uk.ac.aber.cs221.GP01.Main;
+import uk.ac.aber.cs221.GP01.main.java.model.JoggleCube;
+import uk.ac.aber.cs221.GP01.main.java.ui.Dialog;
+import uk.ac.aber.cs221.GP01.main.java.ui.Navigation;
+import uk.ac.aber.cs221.GP01.main.java.ui.ScreenType;
+import uk.ac.aber.cs221.GP01.main.java.ui.controllers.GameView;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class GameViewTest {
 
-    GameView view = GameView.getInstance();
+public class GameViewTest  extends ApplicationTest {
 
+    @Override
+    public void start (Stage stage) throws Exception {
+        Main m = new Main();
+        m.start(stage);
+    }
+
+
+    private GameView view = GameView.getInstance();
 
 
     @BeforeEach
     public void reset() {
-        view.prepView();
-    }
-
-    @Test
-    public void prepViewTest(){
-        assertEquals("0", view.getScoreLabel().getText());
-        assertEquals("3:00", view.getTimerLabel().getText());
-        assertEquals("", view.getText());
-        assertEquals("-fx-text-fill: white;", view.getTimerLabel().getStyle());
+        JoggleCube.getInstance().resetGameState();
+        JoggleCube.getInstance().generateRandomGrid();
+        Platform.runLater(()-> Navigation.getInstance().switchScreen(ScreenType.GAME));
+        Dialog d = GameView.getInstance().getDialog();
+        clickOn(700,320);
     }
 
 
-    @Test
-    public void testBtnClearClicked() {
 
-        assertEquals("", view.getText());
-        for (int x = 0; x < 3; x++)
-            for (int y = 0; y < 3; y++)
-                for (int z = 0; z < 3; z++)
-                    assertTrue(view.getGridDisplayer().isActive(x, y, z));
+        @Test
+        public void prepViewTest(){
+            assertEquals("0", view.getScoreLabel().getText());
+            assertEquals("3:00", view.getTimerLabel().getText());
+            assertEquals("", view.getText());
+            assertEquals("-fx-text-fill: white;", view.getTimerLabel().getStyle());
+        }
+
+        @Test
+        public void testBtnClearClicked() {
+            Platform.runLater(()->{
+                assertEquals("", view.getText());
+                for (int x = 0; x < 3; x++)
+                    for (int y = 0; y < 3; y++)
+                        for (int z = 0; z < 3; z++)
+                            assertTrue(view.getGridDisplayer().isActive(x, y, z));
 
 
-        view.setText("text");
-        view.getGridDisplayer().setInActiveP(1,1,1);
-        view.getGridDisplayer().setInActiveP(2,2,2);
+                view.setText("text");
+                view.getGridDisplayer().setInActiveP(1,1,1);
+                view.getGridDisplayer().setInActiveP(2,2,2);
 
-        assertNotEquals("", view.getText());
+                assertNotEquals("", view.getText());
 
-        view.btnClearClicked();
+                view.btnClearClicked();
 
-        assertEquals("", view.getText());
-        for (int x = 0; x < 3; x++)
-            for (int y = 0; y < 3; y++)
-                for (int z = 0; z < 3; z++)
-                    assertTrue(view.getGridDisplayer().isActive(x, y, z));
-
-    }
-
+                assertEquals("", view.getText());
+                for (int x = 0; x < 3; x++)
+                    for (int y = 0; y < 3; y++)
+                        for (int z = 0; z < 3; z++)
+                            assertTrue(view.getGridDisplayer().isActive(x, y, z));
+            });
+        }
 
     @Test
     public void testBtnSubmitClicked() {
+        Platform.runLater(()-> {
+            view.setText("");
+            assertTrue(view.getFoundWords().isEmpty());
+            view.btnSubmitClicked();
+            view.btnMenuClicked();
 
-        view.setText("");
-        assertTrue(view.getFoundWords().isEmpty());
-        view.btnSubmitClicked();
-        assertTrue(view.getFoundWords().isEmpty());
-        view.setText("text");
-        view.btnSubmitClicked();
-        assertFalse(view.getFoundWords().isEmpty());
+            assertTrue(view.getFoundWords().isEmpty());
+            view.setText("text");
+            view.btnSubmitClicked();
+            assertFalse(view.getFoundWords().isEmpty());
+        });
     }
-
 
     @Test
     public void testBtnMenuClicked() {
+        Platform.runLater(()-> {
+            assertFalse(view.getHamburgerContext().isShowing());
 
-        assertFalse(view.getHamburgerContext().isShowing());
+            view.btnMenuClicked();
 
-        view.btnMenuClicked();
+            assertTrue(view.getHamburgerContext().isShowing());
 
-        assertTrue(view.getHamburgerContext().isShowing());
+            //Point2D screenPos = view.getMenuButton().localToScreen(view.getMenuButton().getLayoutX(), view.getMenuButton().getLayoutY());
+            //assertTrue(view.getHamburgerContext().getX() == screenPos.getX() - 100);
+            //assertTrue(view.getHamburgerContext().getY() == screenPos.getY() + 20);
+        });
+    }
 
-        Point2D screenPos = view.getMenuButton().localToScreen(view.getMenuButton().getLayoutX(), view.getMenuButton().getLayoutY());
-        assertTrue(view.getHamburgerContext().getX() == screenPos.getX() - 100);
-        assertTrue(view.getHamburgerContext().getY() == screenPos.getY() + 20);
+    @Test
+    public void btnEndGameClicked(){
+        Platform.runLater(()->{
+
+
+        });
     }
 }
